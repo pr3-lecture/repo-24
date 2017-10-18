fa20f462b1e7d0be4424fdda0919844e598e3850
Richard Vladimirskij <admin@limecake.com.au>
2017-10-18 10:38:00 +0200

    14/282

--- koans/about_objects.rb
+++ koans/about_objects.rb
@@ -29,17 +29,19 @@ class AboutObjects < Neo::Koan
   def test_every_object_has_different_id
     obj = Object.new
     another_obj = Object.new
-    assert_equal __, obj.object_id != another_obj.object_id
+    assert_equal true, obj.object_id != another_obj.object_id
+    #Should come out as true as long as both have different IDs
   end
 
   def test_small_integers_have_fixed_ids
-    assert_equal __, 0.object_id
-    assert_equal __, 1.object_id
-    assert_equal __, 2.object_id
-    assert_equal __, 100.object_id
+    assert_equal 1, 0.object_id
+    assert_equal 3, 1.object_id
+    assert_equal 5, 2.object_id
+    assert_equal 201, 100.object_id
 
     # THINK ABOUT IT:
     # What pattern do the object IDs for small integers follow?
+    # The pattern used to set the ID for a Number object is 2n+1, where n = the number 
   end
 
   def test_clone_creates_a_different_object
